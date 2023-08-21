1df881b08adcf00222667da7b912560f275f9349
hongseongbin <tjdqls12271@gmail.com>
2023-08-21 18:08:29 +0900

    FEAT:Main.jsx

--- src/Page/Main/Main.jsx
+++ src/Page/Main/Main.jsx
@@ -3,10 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Main = () => {
   return (
-    <>
-    <div>Main</div>
-    <Link to='/searchFood'>link to searchFood</Link>
-    </>
+    <div id='main'>
+        <div className="wrap">
+            <div className="left-container">
+                <div className='content'>content</div>
+            </div>
+            <div className="right-container">
+                <div className="profile"></div>
+                <div className="receipt"></div>
+            </div>
+
+
+        </div> 
+    </div>
   )
 }
 
