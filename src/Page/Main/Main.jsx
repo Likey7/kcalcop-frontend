1dd0de4731960a9eca10b76c66ef243d869f3263
hongseongbin <tjdqls12271@gmail.com>
2023-08-21 16:54:06 +0900

    FEAT:image

--- src/Page/Main/Main.jsx
+++ src/Page/Main/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Main.css'
 
 const Main = () => {
   return (
-    <>
-    <div>Main</div>
+    <div id='main' className='wrap'>Main
+    <img src='/image/logo.svg' alt='로고이미지'/>
     <Link to='/searchFood'>link to searchFood</Link>
-    </>
+    </div>
   )
 }
 
