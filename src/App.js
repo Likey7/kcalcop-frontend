c862259a99d29657b2c9e3de012e4529c92e590f
hongseongbin <tjdqls12271@gmail.com>
2023-08-21 16:38:49 +0900

    FEAT:App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import Main from './Page/Main/Main';
+import { Route, Routes } from 'react-router-dom';
+import SearchFood from './Page/Record/SearchFood';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+   <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/searchFood" element={<SearchFood/>} />
+
+
+   </Routes>
+    </>
   );
 }
 
